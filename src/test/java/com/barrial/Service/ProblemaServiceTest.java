01b2fd7c5ea6f105dab4b0de6af3a9d157e26381
S3basjasan <jaime.sanchez02@epn.edu.ec>
2025-05-10 22:43:25 -0500

    test para busqueda y obtencion de problemas

--- src/test/java/com/barrial/Service/ProblemaServiceTest.java
+++ src/test/java/com/barrial/Service/ProblemaServiceTest.java
@@ -1,10 +1,20 @@
 package com.barrial.Service;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class ProblemaServiceTest {
 
+    @Test
     public void given_when_then(){
+
+
+    }
+
+    @Test
+    public void given_when_then2(){
+
 
     }
 
