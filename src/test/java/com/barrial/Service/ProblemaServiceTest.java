5f710f167b83ba9e79ae5748647305719df22a70
HenryLudenaPyX <ludenahenry2@gmail.com>
2025-05-11 18:06:11 -0500

    Agregando comentarios a Prueba 2 de Mock

--- src/test/java/com/barrial/Service/ProblemaServiceTest.java
+++ src/test/java/com/barrial/Service/ProblemaServiceTest.java
@@ -56,17 +56,23 @@ public class ProblemaServiceTest {
     //Prueba Unitaria Mockito 2: Simular error en base de datos al actualizar votos.
     @Test
     public void given_update_on_BD_when_wrong_data_then_exception(){
+        //Objetos con datos de entrada del usuario
         Problema problema = new Problema(
                 1, "NuevoProblema", "Problema descripción", 3, "http//imagen"
         );
+        //Se mockea al DAO
         ProblemaDAO mockDAO = mock(ProblemaDAO.class);
-        doThrow(new RuntimeException("DB error")).when(mockDAO).editarSeguridad(any(Problema.class));
+        //Simulación de excepción del método editarSeguridad
+        doThrow(new RuntimeException("DB No error")).when(mockDAO).editarSeguridad(any(Problema.class));
 
+        //Verificar que se lanza la excepción esperada
         Exception exception = assertThrows(RuntimeException.class, () -> {
             mockDAO.editarSeguridad(problema);
         });
-
+        //Comprobar que la excepción lanzada es la esperada
         assertEquals("DB error", exception.getMessage());
+
+        System.out.println("Test con Mock");
     }
 
 }
