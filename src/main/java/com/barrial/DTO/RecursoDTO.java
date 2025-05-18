6a2c2738752a7a866bb44e577acac8be5ab32cf1
S3basjasan <jaime.sanchez02@epn.edu.ec>
2025-05-18 15:04:08 -0500

    Test unitarios, de paremetros y mock para recursos

--- src/main/java/com/barrial/DTO/RecursoDTO.java
+++ src/main/java/com/barrial/DTO/RecursoDTO.java
@@ -1,5 +1,47 @@
 package com.barrial.DTO;
 
 public class RecursoDTO {
+    private int id;
+    private String tipoRecurso;
+    private String nombreRecurso;
+    private String cantidadRecurso;
+    private String imagen;
 
+    public RecursoDTO(String tipoRecurso, String nombreRecurso, String cantidadRecurso, String imagen) {
+        this.tipoRecurso = tipoRecurso;
+        this.nombreRecurso = nombreRecurso;
+        this.cantidadRecurso = cantidadRecurso;
+        this.imagen = imagen;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getTipoRecurso() {
+        return tipoRecurso;
+    }
+    public void setTipoRecurso(String tipoRecurso) {
+        this.tipoRecurso = tipoRecurso;
+    }
+    public String getNombreRecurso() {
+        return nombreRecurso;
+    }
+    public void setNombreRecurso(String nombreRecurso) {
+        this.nombreRecurso = nombreRecurso;
+    }
+    public String getCantidadRecurso() {
+        return cantidadRecurso;
+    }
+    public void setCantidadRecurso(String cantidadRecurso) {
+        this.cantidadRecurso = cantidadRecurso;
+    }
+    public String getImagen() {
+        return imagen;
+    }
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
 }
