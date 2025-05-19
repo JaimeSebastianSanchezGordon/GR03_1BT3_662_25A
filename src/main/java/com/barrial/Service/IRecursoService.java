33eaf183a4071c8a0adef0a0fead0fbd192d09c8
OscarTum <oscar.tumbaco01@epn.edu.ec>
2025-05-18 20:39:22 -0500

    Test con Mockito

--- src/main/java/com/barrial/Service/IRecursoService.java
+++ src/main/java/com/barrial/Service/IRecursoService.java
@@ -3,5 +3,7 @@ package com.barrial.Service;
 import com.barrial.Entity.Recurso;
 
 public interface IRecursoService {
+    boolean registrarRecurso(Recurso recurso);
 
+    boolean buscarRecurso(Recurso recurso);
 }
