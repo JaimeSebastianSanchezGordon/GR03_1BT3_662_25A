730157573dc439c6fc2abb516cfb13f41169bf2c
HenryLudenaPyX <ludenahenry2@gmail.com>
2025-05-27 22:38:36 -0500

    Commit fixxing

--- src/main/java/com/barrial/Service/IIntercambioService.java
+++ src/main/java/com/barrial/Service/IIntercambioService.java
@@ -1,9 +1,6 @@
 package com.barrial.Service;
 
-import com.barrial.DTO.IntercambioDTO;
 import com.barrial.Entity.Intercambio;
-
-import java.util.List;
 
 public interface IIntercambioService {
     boolean validacionCamposObligatorio(Intercambio intercambio);
